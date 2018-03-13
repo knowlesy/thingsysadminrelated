4c380fb8bfa13ab20e5550b41381147a6c8d3484
knowlesy <rockpeteuk@gmail.com>
2018-03-13 11:48:47 +0000

    Update DC.ps1

--- Lab/DC.ps1
+++ Lab/DC.ps1
@@ -40,20 +40,15 @@ $userpw = ConvertTo-SecureString “Password1234” -AsPlainText -Force
 #/End Config Variables
 #####################################################################
 
-"This script is running with elevated admin privileges"
-" "
+#####################################################################
+#####################################################################
 
-"Configuring System Settings..."	
-# SYSTEM SETTINGS
 
-    # Create Support Folder(s)
-    mkdir c:\Support
-    mkdir c:\Support\Config
-    mkdir c:\Support\Scripts
-    mkdir c:\Support\BuildFiles
+#####################################################################
+#Functions
+#####################################################################
 
-    #download sysinternals
-    function get-sysinternals
+function get-sysinternals
 {
 $testgoogle = test-connection google.com -Quiet -Count 2
  if ( $testgoogle -eq "TRUE")
@@ -67,6 +62,89 @@ else
 
 
 }
+
+
+function Set-PageFile {
+    <#
+    https://souladin.wordpress.com/2014/03/24/powershell-function-set-pagefile/
+    .SYNOPSIS
+        Sets Page File to custom size
+ 
+    .DESCRIPTION
+        Disables automatic management of the pagefile, then applies the given values for path and page file size.
+        Defaults to C:\pagefile.sys with a 4 gig pagefile.
+ 
+    .PARAMETER Path
+        The page file's fully qualified file name (such as C:\pagefile.sys)
+ 
+    .PARAMETER InitialSize
+        The page file's initial size [MB]
+ 
+    .PARAMETER MaximumSize
+        The page file's maximum size [MB]
+ 
+    .EXAMPLE
+        C:\PS> Set-PageFile "C:\pagefile.sys" 4096 6144
+    #>
+    
+    PARAM(
+        [string]$Path = "C:\pagefile.sys",
+        [int]$InitialSize = 4096,
+        [int]$MaximumSize = 4096
+    )
+     
+    $ComputerSystem = $null
+    $CurrentPageFile = $null
+    $modify = $false
+ 
+    # Disables automatically managed page file setting first
+    $ComputerSystem = Get-WmiObject -Class Win32_ComputerSystem -EnableAllPrivileges
+    if ($ComputerSystem.AutomaticManagedPagefile) {
+        $ComputerSystem.AutomaticManagedPagefile = $false
+        $ComputerSystem.Put()
+    }
+ 
+    $CurrentPageFile = Get-WmiObject -Class Win32_PageFileSetting
+    if ($CurrentPageFile.Name -eq $Path) {
+        # Keeps the existing page file
+        if ($CurrentPageFile.InitialSize -ne $InitialSize) {
+            $CurrentPageFile.InitialSize = $InitialSize
+            $modify = $true
+        }
+        if ($CurrentPageFile.MaximumSize -ne $MaximumSize) {
+            $CurrentPageFile.MaximumSize = $MaximumSize
+            $modify = $true
+        }
+        if ($modify) { $CurrentPageFile.Put() }
+    }
+    else {
+        # Creates a new page file
+        $CurrentPageFile.Delete()
+        Set-WmiInstance -Class Win32_PageFileSetting -Arguments @{Name=$Path; InitialSize = $InitialSize; MaximumSize = $MaximumSize}
+    }
+}
+
+
+#####################################################################
+#/End Functions
+#####################################################################
+
+
+"This script is running with elevated admin privileges"
+" "
+
+"Configuring System Settings..."	
+# SYSTEM SETTINGS
+
+    # Create Support Folder(s)
+    mkdir c:\Support
+    mkdir c:\Support\Config
+    mkdir c:\Support\Scripts
+    mkdir c:\Support\BuildFiles
+
+    #download sysinternals
+    get-sysinternals
+
     
     # Disable Server manager at startup
         New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" –Force
@@ -112,8 +190,7 @@ else
 
 "Configuring Pagefile..."
     #Config Page file
-    #gwmi Win32_PageFileSetting -Arguments @{Name='C:\pagefile.sys'; InitialSize=$pagefile; MaximumSize=$pagefile}
-    wmic pagefileset where name="C:\\pagefile.sys" set InitialSize=$pagefile,MaximumSize=$pagefile
+    Set-PageFile "C:\pagefile.sys" $pagefile $pagefile
 
 "Configuring System Failure Settings..."
     # FAILURE SETTINGS
