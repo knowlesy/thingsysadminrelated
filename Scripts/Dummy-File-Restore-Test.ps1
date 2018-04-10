750ec3448cbb50e90ebaa937e9fb549d6573c730
knowlesy <rockpeteuk@gmail.com>
2018-04-10 10:37:51 +0100

    Create Dummy-File-Restore-Test.ps1

---
+++ Scripts/Dummy-File-Restore-Test.ps1
@@ -0,0 +1,25 @@
+"$datestring = (Get-Date -DisplayHint Date).ToString(""s"").Replace("":"",""-"") 
+$file = ""$datestring.txt"" 
+New-Item -type file ""a:\test\$file"" 
+New-Item -type file ""e:\test\$file"" 
+New-Item -type file ""f:\test\$file"" 
+New-Item -type file ""g:\test\$file"" 
+New-Item -type file ""h:\test\$file"" 
+New-Item -type file ""j:\test\$file"" 
+New-Item -type file ""k:\test\$file"" 
+New-Item -type file ""l:\test\$file"" 
+New-Item -type file ""u:\test\$file"" 
+New-Item -type file ""y:\test\$file"" "							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
