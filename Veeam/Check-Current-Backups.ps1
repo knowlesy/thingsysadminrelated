a66a85ff12f74aa01273f95ac5d989914307347f
knowlesy <rockpeteuk@gmail.com>
2018-04-11 15:20:00 +0100

    Update Check-Current-Backups.ps1

--- Veeam/Check-Current-Backups.ps1
+++ Veeam/Check-Current-Backups.ps1
@@ -1,56 +1,61 @@
 Add-PSSnapin VeeamPSSnapin
 ###VARIABLE###
 $servers = "server1","server2"
-$LogLocation = "\\Server\Veeam$\VeeamLog.log" #Full Log for diagnostics attched to email
-$EmailLog = "\\Server\Veeam\EmailVeeamLog.log" #Cutdown Log > Data improted to a seperate Script as Email
+$LogLocation = "\\server\Veeam$\VeeamLog.log" #Full Log for diagnostics attched to email
+$EmailLog = "\\server\Veeam$\EmailVeeamLog.log" #Cutdown Log > Data improted to a seperate Script as Email
 $timeran = Get-Date
+
 $wherewasthisranfrom = $env:computername
 
 ##############
 
-write-line "Script is Ran from $wherewasthisranfrom at $timeran" >> $LogLocation
+Write-Output "Script is Ran from $wherewasthisranfrom at $timeran" >> $LogLocation
+Write-Output "" >> $LogLocation
 foreach ($server in $servers)
 {
     If (Test-Connection $server -Count 2)
     {
         Connect-VBRServer -Server $server
-        Write-Line "Connecting to $Server" >> $LogLocation
+        Write-Output "Connecting to $Server" >> $LogLocation
         $whatsrunning = Get-VBRJob |Where-Object {$_.IsRunning -eq "True" }|Where-Object {$_.JobType -eq "Backup"}| Format-Table -Property Name,JobType,IsRunning -AutoSize
         
         if (($whatsrunning).length -eq "0")
         {
-            Write-Line "" >> $LogLocation
-            Write-Line "$Server" >> $LogLocation
-            Write-Line "No Jobs running on $server at the time of this report" >> $LogLocation
-            Write-Line "" >> $LogLocation
-            Write-Line "" >> $EmailLog
-            Write-Line "$Server" >> $EmailLog
-            Write-Line "No Jobs running on $server at the time of this report" >> $EmailLog
-            Write-Line "" >> $EmailLog
+            Write-Output "" >> $LogLocation
+            Write-Output "$Server" >> $LogLocation
+            Write-Output "No Jobs running on $server at the time of this report" >> $LogLocation
+            Write-Output "" >> $LogLocation
+            Write-Output "" >> $EmailLog
+            Write-Output "$Server" >> $EmailLog
+            Write-Output "No Jobs running on $server at the time of this report" >> $EmailLog
+            Write-Output "" >> $EmailLog
             #Get-VBRJob
         }
         Else
         {
-            Write-Line "" >> $LogLocation
-            Write-Line "$Server" >> $LogLocation
-            Write-Line "Jobs running on $server at the time of this report" >> $LogLocation
+            Write-Output "" >> $LogLocation
+            Write-Output "$Server" >> $LogLocation
+            Write-Output "Jobs running on $server at the time of this report" >> $LogLocation
             $whatsrunning  >> $LogLocation
-            Write-Line "" >> $LogLocation
-            Write-Line "" >> $EmailLog
-            Write-Line "$Server" >> $EmailLog
-            Write-Line "Jobs running on $server at the time of this report" >> $EmailLog
+            Write-Output "" >> $LogLocation
+            Write-Output "" >> $EmailLog
+            Write-Output "$Server" >> $EmailLog
+            Write-Output "Jobs running on $server at the time of this report" >> $EmailLog
             $whatsrunning  >> $EmailLog
-            Write-Line "" >> $EmailLog
+            Write-Output "" >> $EmailLog
 
         }
         
         Disconnect-VBRServer
-        Write-Line "Disconnecting from $Server" >> $LogLocation
+        Write-Output "Disconnecting from $Server" >> $LogLocation
     }
     Else
     {
-        Write-Line "Could not Connect to $server" >> $LogLocation
-        Write-Line "Could not Connect to $server" >> $EmailLog
+        Write-Output "Could not Connect to $server" >> $LogLocation
+        Write-Output "Could not Connect to $server" >> $EmailLog
     }
-    write-line "Script is Ran from $wherewasthisranfrom at $timeran" >> $EmailLog
+    Write-Output "Script is Ran from $wherewasthisranfrom at $timeran" >> $EmailLog
 }
+$completetime = Get-Date
+Write-Output "" >> $LogLocation
+Write-Output "Script Complete $completetime" >> $LogLocation
