d4e94833460846eb9dde590cba0576f74c77e269
knowlesy <rockpeteuk@gmail.com>
2018-04-11 14:47:36 +0100

    Update Check-Current-Backups.ps1

--- Veeam/Check-Current-Backups.ps1
+++ Veeam/Check-Current-Backups.ps1
@@ -1,35 +1,41 @@
 Add-PSSnapin VeeamPSSnapin
 ###VARIABLE###
 $servers = "SERVER1","SERVER2"
+$LogLocation = "<LOCATION>"
 ##############
+
 
 foreach ($server in $servers)
 {
     If (Test-Connection $server -Count 2)
     {
         Connect-VBRServer -Server $server
+        Write-Line "Connecting to $Server" >> $LogLocation
         $whatsrunning = Get-VBRJob |Where-Object {$_.IsRunning -eq "True" }|Where-Object {$_.JobType -eq "Backup"}| Format-Table -Property Name,JobType,IsRunning -AutoSize
         
         if (($whatsrunning).length -eq "0")
         {
-            Write-Host ""
-            write-host "No Jobs running on $server at the time of this report"
-            Write-Host ""
+            Write-Line "" >> $LogLocation
+            Write-Line "$Server" >> $LogLocation
+            Write-Line "No Jobs running on $server at the time of this report" >> $LogLocation
+            Write-Line "" >> $LogLocation
             #Get-VBRJob
         }
         Else
         {
-            Write-Host ""
-            write-host "Jobs running on $server at the time of this report"
-            $whatsrunning
-            Write-Host ""
+            Write-Line "" >> $LogLocation
+            Write-Line "$Server" >> $LogLocation
+            Write-Line "Jobs running on $server at the time of this report" >> $LogLocation
+            $whatsrunning  >> $LogLocation
+            Write-Line "" >> $LogLocation
 
         }
         
         Disconnect-VBRServer
+        Write-Line "Disconnecting from $Server" >> $LogLocation
     }
     Else
     {
-        Write-Host "Could not Connect to $server"
+        Write-Line "Could not Connect to $server" >> $LogLocation
     }
 }
