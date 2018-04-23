c006166501bddfde4b7208c207b546486dc62147
knowlesy <rockpeteuk@gmail.com>
2018-04-23 22:54:57 +0100

    Create Initialinstall.sh

---
+++ Initialinstall.sh
@@ -0,0 +1,15 @@
+yum update
+yum update kernel
+yum update && yum upgrade
+yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install bash-completion bash-completion-extras
+locate bash-completion.sh
+source /etc/profile.d/bash_completion.sh  
+yum install wget
+yum install p7zip
+yum install nano
+yum -y install docker
+yum install -y hyperv-daemons
+echo noop > /sys/block/sda/queue/scheduler
+hostnamectl set-hostname "CentOS-Docker"
+shutdown -r now
