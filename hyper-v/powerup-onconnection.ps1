2a839454310b5f7439f2c01ee5053ea6fc5d5d0f
knowlesy <rockpeteuk@gmail.com>
2018-04-29 10:40:59 +0100

    Update powerup-onconnection.ps1

--- hyper-v/powerup-onconnection.ps1
+++ hyper-v/powerup-onconnection.ps1
@@ -1,9 +1,14 @@
-$vm = 'vm1','vm2'
-$HostName = "hyperv-host"
-$pingHost = "iscsi-or-smbshare-or-whatevs"
+$vm = 'vm1', 'vm2'
+$HostName = "host"
+$pingHost = 'san-isci-smb'
 Import-Module Hyper-V
-$ping = Test-Connection $pingHost -Count 2
-If ($pingHost)
+$ping = Test-Connection $pingHost -Count 2 -Quiet
+If ($ping)
 {
     Start-VM -ComputerName $HostName -Name $vm
+    Write-EventLog -LogName Application -Source "Scripts" -EntryType Information -EventId 1 -Message "$vm Powered On."
+}
+else
+{
+    Write-EventLog -LogName Application -Source "Scripts" -EntryType Warning -EventId 2 -Message "Could not find $pingHost."
 }
