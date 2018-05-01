18836c10be6283af03930f330b67aa114d7c7e1f
knowlesy <rockpeteuk@gmail.com>
2018-05-01 21:32:47 +0100

    Update powerup-onconnection.ps1

--- hyper-v/powerup-onconnection.ps1
+++ hyper-v/powerup-onconnection.ps1
@@ -5,8 +5,17 @@ Import-Module Hyper-V
 $ping = Test-Connection $pingHost -Count 2 -Quiet
 If ($ping)
 {
-    Start-VM -ComputerName $HostName -Name $vm
-    Write-EventLog -LogName Application -Source "Scripts" -EntryType Information -EventId 1 -Message "$vm Powered On."
+     $aretheyonline = get-vm -Name $vm | where {$_.State -eq "Running"} | Select-Object -ExpandProperty Name 
+
+    if ($aretheyonline.ToString() -contains $vm.ToString())
+    {
+        Write-EventLog -LogName Application -Source "Scripts" -EntryType Information -EventId 1 -Message "$vm Already online."
+    }
+    Else
+    {
+        Start-VM -ComputerName $HostName -Name $vm
+        Write-EventLog -LogName Application -Source "Scripts" -EntryType Information -EventId 1 -Message "$vm Powered On."
+    }
 }
 else
 {
