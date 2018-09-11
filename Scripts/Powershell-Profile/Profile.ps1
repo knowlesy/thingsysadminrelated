19795310022a2c4e4a68faaad758450d7471333d
Knowles <knowlesp@KERRIDGECS.LOCAL>
2018-09-11 13:10:09 +0100

    update

--- Scripts/Powershell-Profile/Profile.ps1
+++ Scripts/Powershell-Profile/Profile.ps1
@@ -1,6 +1,6 @@
 set-location c:
 Start-Transcript -OutputDirectory "C:\Support\transcripts\" -NoClobber
-new-item alias:np -value "C:\Program Files (x86)\Notepad++\notepad++.exe"
+new-item alias:np -value "C:\Program Files\Notepad++\notepad++.exe"
 #Looking up scripts to output in write host
 $items = Get-ChildItem -Path "C:\Support\Scripts\" -filter "*.ps1"
 #Directory where my scripts are stored
@@ -11,4 +11,8 @@ Unblock-File "${psdir}\*.ps1" | % {.$_}
 Get-ChildItem "${psdir}\*.ps1" | % {.$_} 
 #setting root location for powershell window
 set-location c:\
+#Setting colour (used to identify when running as admin)
+#$console = $host.UI.RawUI
+#$console.ForegroundColor = "White"
+#$console.BackgroundColor = "black"
 clear-host
--- Scripts/Reporting/localadmin.ps1
+++ Scripts/Reporting/localadmin.ps1
@@ -1,23 +1,22 @@
 #https://clintboessen.blogspot.com/2018/08/powershell-list-local-administrators-on.html
 
 
-$serverlist = Get-Content C:\Scripts\serverlist.txt
+$serverlist = Get-Content C:\Support\Scripts\serverlist.txt
 
 foreach ($server in $serverlist)
     {
     $ipAddress = $pingStatus.ProtocolAddress;
     # Ping the computer
     $pingStatus = Get-WmiObject -Class Win32_PingStatus -Filter "Address = '$server'";
-    if($pingStatus.StatusCode -eq 0)
-        {
+    if ($pingStatus.StatusCode -eq 0) {
         Write-Host -ForegroundColor Green "Ping Reply received from $server.";
-        $server | Out-File -NoClobber -Append C:\Users\clint-b\localadmins.txt
+        $server | Out-File -NoClobber -Append C:\Support\Logs\localadmins.txt
         $admins = Gwmi win32_groupuser –computer $server
         $admins = $admins |? {$_.groupcomponent –like '*"Administrators"'}
-        $admins |? {$_.groupcomponent –like '*"Administrators"'} | fl *PartComponent* | Out-File -NoClobber -Append C:\Users\clint-b\localadmins.txt
-        }
-    else
-    {
-    Write-Host -ForegroundColor Red "No Ping Reply received from $server.";
+        $admins |? {$_.groupcomponent –like '*"Administrators"'} | fl *PartComponent* | Out-File -NoClobber -Append C:\Support\Logs\localadmins.txt
     }
+    else {
+        Write-Host -ForegroundColor Red "No Ping Reply received from $server.";
+    }
+    
     }
