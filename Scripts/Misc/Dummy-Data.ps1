3bc92c8bfd1e72b2a0f267b394bc263535d1b92f
Knowles <knowlesp@KERRIDGECS.LOCAL>
2018-09-17 17:40:24 +0100

    update

--- Scripts/Misc/Dummy-Data.ps1
+++ Scripts/Misc/Dummy-Data.ps1
@@ -2,24 +2,33 @@
 #Ref https://mcpmag.com/articles/2016/04/07/powershell-functions-with-parameters.aspx
 # needs to be ran elavated    
 
-function start-fakedata ($filename)
-      #start-fakedata = "C:\temp\test"
-{
-   $DaysToMove=((Get-Random 120) -60)
-   $HoursToMove=((Get-Random 48) -24)
-   $MinutesToMove=((Get-Random 120) -60)
-   $TimeSpan=New-TimeSpan -Days $DaysToMove -Hours $HoursToMove -Minutes $MinutesToMove
+function Start-FakeData {
 
-   # Now we adjust the Date and Time by the new TimeSpan
-   # Needs Admin rights to do this as well!
+    [CmdletBinding()]
 
-     Set-Date -Adjust $Timespan | Out-Null
+    param(
 
-   # Create that file
-   Add-Content -Value $a -Path $filename
+        [Parameter()]
 
-   # Now we REVERSE the Timespan by the exact same amount
-   $TimeSpan=New-TimeSpan -Days (-$DaysToMove) -Hours (-$HoursToMove) -Minutes (-$MinutesToMove)
+        [string]$FolderPath
 
-   Set-Date -Adjust ($Timespan) | Out-Null
+    )
+
+    $DaysToMove = ((Get-Random 120) - 60)
+    $HoursToMove = ((Get-Random 48) - 24)
+    $MinutesToMove = ((Get-Random 120) - 60)
+    $TimeSpan = New-TimeSpan -Days $DaysToMove -Hours $HoursToMove -Minutes $MinutesToMove
+
+    # Now we adjust the Date and Time by the new TimeSpan
+    # Needs Admin rights to do this as well!
+
+    Set-Date -Adjust $Timespan | Out-Null
+
+    # Create that file
+    Add-Content -Value $a -Path $FolderPath
+    # Now we REVERSE the Timespan by the exact same amount
+    $TimeSpan = New-TimeSpan -Days (-$DaysToMove) -Hours (-$HoursToMove) -Minutes (-$MinutesToMove)
+
+    Set-Date -Adjust ($Timespan) | Out-Null
+
 }
