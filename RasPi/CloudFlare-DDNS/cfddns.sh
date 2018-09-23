e5eec0f2f0a67c456cf82962d5dfbc509b439aa9
knowlesy <rockpeteuk@gmail.com>
2018-09-23 20:30:43 +0100

    Create cfddns.sh

---
+++ RasPi/CloudFlare-DDNS/cfddns.sh
@@ -0,0 +1,22 @@
+#REF https://ghostpi.pro/cloudflare-ddns-raspberry-pi/
+# use code below errors on main page for final script 
+
+
+#!/bin/sh
+
+[ ! -f /scripts/current_ip.txt ] && touch /scripts/currentip.txt
+
+NEWIP=$(curl icanhazip.com)
+CURRENTIP='cat /scripts/currentip.txt'
+
+if [ "$NEWIP" = "$CURRENTIP" ]
+then
+  echo "IP address unchanged"
+else
+  curl -X PUT "https://api.cloudflare.com/client/v4/zones/<zoneid>/dns_records/<recordid>" \
+    -H "X-Auth-Email: name@email.com" \
+    -H "X-Auth-Key: <apikey>" \
+    -H "Content-Type: application/json" \
+    --data "{\"type\":\"A\",\"name\":\"123.domain.net\",\"content\":\"$NEWIP\"}"
+  echo $NEWIP > /scripts/currentip.txt
+fi
