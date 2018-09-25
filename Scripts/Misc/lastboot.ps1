988e446424f7b70800b6fa6c92299c9506da1c22
Knowles <knowlesp@KERRIDGECS.LOCAL>
2018-09-25 09:21:57 +0100

    update

--- Scripts/Misc/lastboot.ps1
+++ Scripts/Misc/lastboot.ps1
@@ -4,6 +4,10 @@ $compname = Get-Content -Path C:\Support\info\Servers.txt
 foreach ($comp in $compname) {
     $errorwithpc = $comp + ' Can not connect'
     $logoutput = 'C:\Support\Logs\lastboot.log'
-    Get-WmiObject win32_operatingsystem -ComputerName $comp| select CSName, @{LABEL = 'LastBootUpTime'; EXPRESSION = {$_.ConverttoDateTime($_.lastbootuptime)}} -ErrorVariable $errorwithpc | Out-File -FilePath $logoutput -Append
+    Get-WmiObject win32_operatingsystem -ComputerName $comp -ErrorAction SilentlyContinue -ErrorVariable $erroroccured | Select-Object CSName, @{LABEL = 'LastBootUpTime'; EXPRESSION = {$_.ConverttoDateTime($_.lastbootuptime)}}| Out-File -FilePath $logoutput -Append
+    If ($erroroccured) {
 
+        $errorwithpc | Out-File -FilePath $logoutput -Append
+
+    }
 }
