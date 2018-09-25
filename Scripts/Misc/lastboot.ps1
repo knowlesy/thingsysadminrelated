4b6929061631e2c5a3980e15b0240cb9b7192fad
Knowles <knowlesp@KERRIDGECS.LOCAL>
2018-09-25 09:09:22 +0100

    update

--- Scripts/Misc/lastboot.ps1
+++ Scripts/Misc/lastboot.ps1
@@ -1,7 +1,9 @@
 #REF
 #https://serverfault.com/questions/722687/script-for-getting-last-reboot-timestamp-2008r2
-$compname = Get-Content -Path C:\Support\info\test.txt
+$compname = Get-Content -Path C:\Support\info\Servers.txt
 foreach ($comp in $compname) {
-    Get-WmiObject win32_operatingsystem -ComputerName $comp| select CSName, @{LABEL = 'LastBootUpTime'; EXPRESSION = {$_.ConverttoDateTime($_.lastbootuptime)}}  | Out-File C:\Logs\lastboot.log -Append
+    $errorwithpc = $comp + ' Can not connect'
+    $logoutput = 'C:\Support\Logs\lastboot.log'
+    Get-WmiObject win32_operatingsystem -ComputerName $comp| select CSName, @{LABEL = 'LastBootUpTime'; EXPRESSION = {$_.ConverttoDateTime($_.lastbootuptime)}} -ErrorVariable $errorwithpc | Out-File -FilePath $logoutput -Append
 
 }
