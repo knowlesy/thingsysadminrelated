6a86a28673e2aef5c9858c19377a06e28fcbb63e
Knowles <knowlesp@KERRIDGECS.LOCAL>
2018-10-18 09:31:42 +0100

    update

--- Scripts/Cleaning-Machines/Kill.ps1
+++ Scripts/Cleaning-Machines/Kill.ps1
@@ -1,52 +1,53 @@
 
 mkdir C:\temp\tmp\tmp1
 mkdir C:\temp\tmp\tmp2
+
+
+$final = 'C:\temp\tmp\tmp2'
 $temp = 'C:\temp\tmp\tmp1'
-$who = $env:USERNAME
-$where = 'C:\Users\'+ $who
+#$who = $env:USERNAME
+#$where = 'C:\Users\'+ $who
+$where = 'C:\Temp\Test'
+
 
 function get-pzero {
     if (test-connection google.com -Quiet -Count 2) { 
         Write-Host Connected to outside world.......Downloading zero;
-        #https://gallery.technet.microsoft.com/scriptcenter/a6b10a18-c4e4-46cc-b710-4bd7fa606f95
-        (New-Object Net.WebClient).DownloadFile('https://download.sysinternals.com/files/SDelete.zip', 'C:\temp\tmp\SDelete.zip'); (new-object -com shell.application).namespace('C:\temp\tmp\tmp2').CopyHere((new-object -com shell.application).namespace('C:\temp\tmp\SDelete.zip').Items(), 16)
+        (New-Object Net.WebClient).DownloadFile('https://download.sysinternals.com/files/SDelete.zip', 'C:\temp\tmp\temp1.zip'); (new-object -com shell.application).namespace('C:\temp\tmp\tmp2').CopyHere((new-object -com shell.application).namespace('C:\temp\tmp\temp1.zip').Items(), 16)
+        (New-Object Net.WebClient).DownloadFile('https://download.sysinternals.com/files/PSTools.zip', 'C:\temp\tmp\temp2.zip'); (new-object -com shell.application).namespace('C:\temp\tmp\tmp2').CopyHere((new-object -com shell.application).namespace('C:\temp\tmp\temp2.zip').Items(), 16)
     }
     else
     {write-host Not Downloading zero}
 }
 
-function get-exec {
-    if (test-connection google.com -Quiet -Count 2) { 
-        Write-Host Connected to outside world.......Downloading zero;
-        #https://gallery.technet.microsoft.com/scriptcenter/a6b10a18-c4e4-46cc-b710-4bd7fa606f95
-        (New-Object Net.WebClient).DownloadFile('https://download.sysinternals.com/files/PSTools.zip', 'C:\temp\tmp\exec.zip'); (new-object -com shell.application).namespace('C:\temp\tmp\tmp2').CopyHere((new-object -com shell.application).namespace('C:\temp\tmp\exec.zip').Items(), 16)
-    }
-    else
-    {write-host Not Downloading zero}
-}
 
 get-pzero
-get-exec
 
-Remove-Item –path C:\temp\tmp\SDelete.zip
-Remove-Item –path C:\temp\tmp\exec.zip
+
+Remove-Item –path C:\temp\tmp -Include *.zip -Recurse
+
 
 $zero = {
-    C:\temp\tmp\tmp2\sdelete64.exe C: -q -z
-   
-  }
+    wevtutil el | Foreach-Object {wevtutil cl “$_”}
+    C:\temp\tmp\tmp2\sdelete64.exe -q -s $where
+    C:\temp\tmp\tmp2\sdelete64.exe -q -s C:\Support
+    C:\temp\tmp\tmp2\PsExec64.exe -s Robocopy $temp $where /mir /r:1 /w:1 /mt:10
 
-$remove = {
-    
-    C:\temp\tmp\tmp2\PsExec64.exe -s -i Robocopy $temp $where /mir /r:1 /w:1 /mt:10
-   
-}
+     }
+
+
 $clean = {
-    Remove-Item -Path C:\temp\tmp\tmp2\*
-    C:\temp\tmp\tmp2\sdelete64.exe $where -q -r
+     C:\temp\tmp\tmp2\sdelete64.exe -z c:
+    Robocopy $final $temp /mir /r:1 /w:1 /mt:10 
+    Remove-Item –path C:\Support -Recurse
+    wevtutil el | Foreach-Object {wevtutil cl “$_”}
 }
 
 
   
   Start-Job $zero
-  Start-Job $remove
+
+
+  wait-job -name ss64 -timeout 300
+
+  start-job $clean
