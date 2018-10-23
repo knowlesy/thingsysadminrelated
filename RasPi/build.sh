6452baa714da494add64ccfea5095b6e32ec6800
Knowlesy <rockpeteuk@gmail.com>
2018-10-23 20:38:32 +0100

    update

--- RasPi/build.sh
+++ RasPi/build.sh
@@ -1,6 +1,25 @@
 sudo su
+df -h
+mkdir logs
+mkdir scripts
 apt-get update
+apt-get upgrade
+apt-get dist-upgrade
+rpi-update
 apt-get install wget
 apt-get install dnsutils
 apt-get install p7zip
+apt-get install iftop
+apt-get install fail2ban
+service fail2ban start
+cp /etc/fail2ban/jail.conf
+service fail2ban restart
+fail2ban-client status
+sudo passwd -dl root
+raspi-config
 shutdown -r now
+sudo su
+curl -sSL https://install.pi-hole.net | bash
+shutdown -r now
+sudo su
+apt-get install openvpn
