9d17ef07984f6e1cdbeb50814a1d3a19fa19ec4b
Knowlesy <rockpeteuk@gmail.com>
2018-10-26 18:44:18 +0100

    update

---
+++ RasPi/clearip.sh
@@ -0,0 +1,9 @@
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
