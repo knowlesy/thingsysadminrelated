142c376d658e47c46b6090c2b35809a15cf3bb37
knowlesy <rockpeteuk@gmail.com>
2019-01-15 15:03:15 +0000

    Update dsfr-check.ps1

--- Scripts/File Server/dsfr-check.ps1
+++ Scripts/File Server/dsfr-check.ps1
@@ -1,4 +1,5 @@
 #REF bwatt
+#REF https://www.reddit.com/r/PowerShell/comments/ad0egr/check_dfs_backlog/?utm_source=reddit-android
 # List of DFSR Source/Target pairs to monitor
 $DFSRPaths = @{
     "server\share" = "server\share";
@@ -13,6 +14,20 @@ $smtpServer = "smtpserver"
 $smtpFrom = "noemail@company.com"
 $emailDate = Get-Date -Format "dd/MM/yyyy"
 
+Remove-Item -Path C:\scripts\Orchestration\Daily\DFRS\logcount.txt -Force
+$RGGroups = Get-DfsReplicationGroup 
+#"Displaying Replication Backlog"
+foreach ( $RG in $RGGroups) {
+    $RFolders = Get-DfsReplicatedFolder -GroupName $RG.GroupName
+    foreach ($RF in $RFolders) {
+        $RCons = Get-DfsrConnection -GroupName $RG.GroupName 
+        foreach ($RC in $RCons) {
+             $date = get-date -f "yyyy-MM-dd HH:mm:ss"
+            [double]$count = (Get-DFSRBackLog -groupName $RG.GroupName -folderName $RF.FolderName -DestinationComputerName $RC.DestinationComputerName -SourceComputerName $RC.SourceComputerName -verbose 4>&1).Message.split(":")[2]
+            "[{0}] RG: {1} RF: {2} on {3} to {4} Backlog Length: {5}" -f $date,$RG.GroupName,$RF.FolderName,$RC.SourceComputerName, $Rc.DestinationComputerName,$count | Out-File -FilePath "C:\scripts\Orchestration\Daily\DFRS\logcount.txt" -Append
+        }
+    }
+}
 
 # creates a file if it does not already exist
 Function Touch-File {
@@ -37,6 +52,7 @@ Function Check-All {
     $textFailures = ""
     $textSuccessesHeading = "Replication successful"
     $textSuccesses = ""
+    $textbacklog = Get-Content -Path C:\scripts\Orchestration\Daily\DFRS\logcount.txt -Raw
     Write-Host "Checking target files"
     foreach ($key in $DFSRPaths.Keys) { 
         $modTime = (Get-Date).Addhours(-25)
@@ -52,7 +68,7 @@ Function Check-All {
     }
 
     $messageSubject = "DFS Replication statuses - $emailDate"
-    $messageBody = "$textFailureHeading`r`n$textFailures`r`n`r`n$textSuccessesHeading`r`n$textSuccesses"
+    $messageBody = "$textFailureHeading`r`n$textFailures`r`n`r`n$textSuccessesHeading`r`n$textSuccesses`r`n`r $textbacklog `r`n`r"
 	
     Send-MailMessage -to $toRecipients -cc $ccRecipients -from $smtpFrom -Subject $messageSubject -body $messageBody -smtpServer $smtpServer
 }
