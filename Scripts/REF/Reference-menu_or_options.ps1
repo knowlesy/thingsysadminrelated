e6cf67081c10e5263695f0e9ebcb6a888bb4585e
knowlesy <rockpeteuk@gmail.com>
2019-09-27 10:06:45 +0100

    Update Reference-menu_or_options.ps1

--- Scripts/REF/Reference-menu_or_options.ps1
+++ Scripts/REF/Reference-menu_or_options.ps1
@@ -1,10 +1,11 @@
 #REF 
 #https://www.petri.com/building-a-powershell-console-menu-revisited-part-1
 #https://4sysops.com/archives/how-to-build-an-interactive-menu-with-powershell/
+#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_switch?view=powershell-6
 #Testing switch commands in powershell 
 
 
-$number = Read-Host "Enter a number 1-4 or A for all"
+$number = Read-Host "Enter a number 1-4 or A for all or Q for quit"
 write-host ("Variable entered " + $number)
 #if it matches a then run all of the switches
 if ($number -eq "A")
@@ -16,10 +17,16 @@ if ($number -eq "A")
     write-host ("Variable updated now contains " + $number)
 
 }
+
 switch($number){
     1 {"One"}
     2 {"Two"}
+    #below will stop looking after it is found break causes an end
+    #3 {"Three" ; Break}
     3 {"Three"}
     4 {"Four"}
     3 {"Three Again"}
+    1 {"One"}
+    'q' {write-host "quitting"
+        exit}
  }
