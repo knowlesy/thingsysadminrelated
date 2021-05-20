5c6f8b888fea4d7e5e22b70755abe3eb7f55f146
knowlesy <rockpeteuk@gmail.com>
2021-05-20 13:29:44 +0100

    Create test2.ps1

---
+++ Azure/ARM/Test/test2.ps1
@@ -0,0 +1,21 @@
+$temp = ".\tagsexample.json"
+$who = read-host "Enter Customer"
+$who = $who.Remove(" ","")
+$sla = read-host "Enter Customer"
+$sla = $sla.Remove(" ","")
+$date = get-date -Format yyyy-MM-dd-hh-mm
+$deployment = ($date + '_' + $who)
+$creator = Get-AzContext
+$customerbillingcode = "1234-$who"
+$projectcode = "Test_Deployment_For_$who"
+$batch = "1","2","3","4"
+$batchselection = Get-Random $batch
+$resourceGroupName = ('rg-uat-vm-' + $who + '-application-1') 
+New-AzResourceGroupDeployment -Name $deployment -ResourceGroupName $resourceGroupName -TemplateFile $temp -user $creator.account `
+ -custbc $customerbillingcode -location 'uksouth' `
+ -projectc $projectcode `
+ -pend $date.AddMonths(6) `
+ -review $date.AddMonths(3) `
+ -updatebatch $batchselection `
+ -sla $sla `
+-customer $who
