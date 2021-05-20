ba8dd395c8e0c92260a3d4674886911370d56ded
Knowlesy <rockpeteuk@gmail.com>
2021-05-20 13:54:19 +0100

    update

--- Azure/ARM/Test/deploy2.ps1
+++ Azure/ARM/Test/deploy2.ps1
@@ -1,21 +1,26 @@
-$temp = ".\tagsexample.json"
+$temp = ".\test7.json"
 $who = read-host "Enter Customer"
-$who = $who.Remove(" ","")
 $sla = read-host "Enter SLA"
-$sla = $sla.Remove(" ","")
-$date = get-date -Format yyyy-MM-dd-hh-mm
-$deployment = ($date + '_' + $who)
+$date = get-date
+$datestring = $date.ToString("yyyyMMddhhmm")
+$deployment = ($datestring + $who)
 $creator = Get-AzContext
 $customerbillingcode = "1234-$who"
 $projectcode = "Test_Deployment_For_$who"
 $batch = "1","2","3","4"
 $batchselection = Get-Random $batch
-$resourceGroupName = ('rg-uat-vm-' + $who + '-application-1') 
+$resourceGroupName = ('rg-uat-vm-' + $who + '-application-1')
+$location = 'uksouth'
+$6months = $date.AddMonths(6)
+$3months = $date.AddMonths(3)
+$projectenddate = $6months.ToString("yyyyMMddhhmm")
+$reviewdate = $3months.ToString("yyyyMMddhhmm")
+New-AzResourceGroup -Name $resourceGroupName -Location $location
 New-AzResourceGroupDeployment -Name $deployment -ResourceGroupName $resourceGroupName -TemplateFile $temp -user $creator.account `
- -custbc $customerbillingcode -location 'uksouth' `
+    -custbc $customerbillingcode -location $location `
  -projectc $projectcode `
- -pend $date.AddMonths(6) `
- -review $date.AddMonths(3) `
+ -pend $projectenddate `
+ -review $reviewdate `
  -updatebatch $batchselection `
  -sla $sla `
--customer $who
+    -customer $who -timec $datestring
