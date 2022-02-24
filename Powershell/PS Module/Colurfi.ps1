be719a1fff905a2d10398665ec8ece0f73df56ca
knowlesy <rockpeteuk@gmail.com>
2022-02-24 23:51:18 +0000

    update

--- Powershell/PS Module/Colurfi.ps1
+++ Powershell/PS Module/Colurfi.ps1
@@ -7,46 +7,46 @@ function Get-textcolour {
     $AUpper = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
     $ALower = "a", "c", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
     $Num = "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-$chars = $text.ToCharArray()
-#cycling through each of the chars in the array 
-foreach ($char in $chars) {
-    #sets exit ppoint of the for 
+    $chars = $text.ToCharArray()
+    #cycling through each of the chars in the array 
+    foreach ($char in $chars) {
+        #sets exit ppoint of the for 
 
-    #uppercase
-    foreach ($au in $AUpper) {
-        if ($char -ceq $au) {
-            write-host $char -ForegroundColor DarkGreen -NoNewline
+        #uppercase
+        foreach ($au in $AUpper) {
+            if ($char -ceq $au) {
+                write-host $char -ForegroundColor DarkGreen -NoNewline
 
+            }
         }
-    }
-    #lowercase
+        #lowercase
 
-    foreach ($al in $ALower) {
-        if ($char -ceq $al) {
-            write-host $char -ForegroundColor Yellow -NoNewline
+        foreach ($al in $ALower) {
+            if ($char -ceq $al) {
+                write-host $char -ForegroundColor Yellow -NoNewline
 
+            }
         }
-    }
 
-    #num
+        #num
 
-    foreach ($n in $Num) {
-        if ($char -eq $n) {
-            write-host $char -ForegroundColor Magenta -NoNewline
+        foreach ($n in $Num) {
+            if ($char -eq $n) {
+                write-host $char -ForegroundColor Magenta -NoNewline
 
+            }
         }
-    }
 
-    #symbol
+        #symbol
 
-    foreach ($sym in $Symbol) {
+        foreach ($sym in $Symbol) {
 
-        if ($char -eq $sym) {
-            write-host $char -ForegroundColor Blue -NoNewline
+            if ($char -eq $sym) {
+                write-host $char -ForegroundColor Blue -NoNewline
                 
+            }
         }
-    }
 
-}
-write-host ""
+    }
+    write-host ""
 }
