fbb150849d342e40b56ca866b1fadc1214c5a9be
knowlesy <rockpeteuk@gmail.com>
2022-02-24 23:49:01 +0000

    update

--- Powershell/PS Module/Colour Password Module.ps1
+++ Powershell/PS Module/Colour Password Module.ps1
@@ -37,8 +37,8 @@ function Get-Password {
         #sets length of passwords 
         for ($i = 1; $i -le $Length; $i++) {
             #char sets 
-            $AUpper = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-            $ALower = "a", "c", "b", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+            $AUpper = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+            $ALower = "a", "c", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
             $Num = "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
             #gets random of each char array 
             $randomupper = Get-Random $AUpper
--- Powershell/PS Module/Password.ps1
+++ Powershell/PS Module/Password.ps1
@@ -33,8 +33,8 @@ function Get-Password {
     for ($m = 1; $m -le $times; $m++)
 {
 for ($i = 1; $i -le $Length; $i++) {
-    $AUpper = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
-    $ALower = "a", "c", "b", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    $AUpper = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+    $ALower = "a", "c", "b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
     $Num = "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
     $randomupper = Get-Random $AUpper
     $randomlower = Get-Random $ALower
