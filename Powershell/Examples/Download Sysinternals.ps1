61d8b0f8647a117cecd5ae655225534bf35ba022
knowlesy <rockpeteuk@gmail.com>
2022-05-01 19:22:06 +0100

    added new example

--- Powershell/Examples/Download Sysinternals.ps1
+++ Powershell/Examples/Download Sysinternals.ps1
@@ -1,11 +1,11 @@
 function get-sysinternals {
     if (test-connection google.com -Quiet -Count 2) { 
-        Write-Host Connected to outside world.......Downloading zero;
+        Write-Host Connected to outside world.......Downloading Sysinternals;
            (New-Object Net.WebClient).DownloadFile('https://download.sysinternals.com/files/SysinternalsSuite.zip', 'C:\support\temp\sysinternals.zip')
            (new-object -com shell.application).namespace('C:\support\tools\').CopyHere((new-object -com shell.application).namespace('C:\support\temp\sysinternals.zip').Items(), 16)
     }
     else
-    {write-host Not Downloading zero}
+    {write-host Not Downloading Sysinternals}
 }
 
 
---
+++ Powershell/Examples/Run a Job in the background and wait.ps1
@@ -0,0 +1,26 @@
+Start-Job -ScriptBlock { C:\support\tools\sdelete64.exe -accepteula -nobanner -z c:}
+
+$wait = Get-Process "sdelete64" -ErrorAction SilentlyContinue| select-object *
+$z = 0
+$zmax = 40 
+if ($wait.Responding -eq "True")
+{
+    Write-Host "SDELETE is running this wil ltake some time"
+    while ($wait.Responding -eq "True") {
+        if ($z -eq $zmax) {
+            write-host "Zeroing ran for over 20 mintes terminating process"
+            stop-process $wait.Id -force
+        }
+        else {
+            write-host "Zeroing C Drive ($z/$zmax)"
+            start-sleep -seconds 30
+            $wait = Get-Process "sdelete64" | select-object *
+            $z++
+        }
+    }
+}
+else
+{ 
+    Write-Host "SDELETE failed to run"
+    #stop-process $wait.Id -force -ErrorAction SilentlyContinue
+}
